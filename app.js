8fe418a12e7ed570f92da4f9b722515070348bf9
Yosef Awad <yosef.awad.1@gmail.com>
2024-04-03 23:54:31 +0300

    no comments

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ const express = require('express');
 
 const app = express();
 const server = http.createServer(app);
-// const server = https.createServer({
-//     cert: fs.readFileSync('./keys/cert.pem'),
-//     key: fs.readFileSync('./keys/key.pem')
-// }, app);
 
 const wss = new WebSocket.Server({ server });
 
 app.use(express.static('public'));
-
 
 
 wss.on('connection', function connection(ws) {
